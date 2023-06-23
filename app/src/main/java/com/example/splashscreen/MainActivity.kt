ae679276bcb203440326f54f1c10707e2b7965a4
Oktay Genç <gncoktay3866@gmail.com>
2023-06-23 23:30:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.splashscreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private val splashTimeOut: Long = 3000
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
+        findViewById<ImageView>(R.id.splashScreen).startAnimation(animation)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashTimeOut)
+            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+            finish()
+        }
+
+    }
+}
